2875de44a7fff4f05c737a352bef591c9a0cc81c
unknown <lakumkeval2004@gmail.com>
2023-06-29 15:09:37 +0530

    dropdown complete

--- script/cart.js
+++ script/cart.js
@@ -1,3 +1,25 @@
 import navbar from "../components/navbar.js";
 
+
+function name(params) {
+    
+}
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById("nav").innerHTML=navbar()
